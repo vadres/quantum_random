10a7cce44573a3c76dc38fc44bd5100d5ef3a470
Victor Costa <antonio.vcp@isgh.org.br>
2021-01-22 15:02:26 -0300

    better categorizer

--- src/app/services/categorizerJson/categorizerJson.ts
+++ src/app/services/categorizerJson/categorizerJson.ts
@@ -2,9 +2,15 @@ import { Categorizer } from "@/domain/usecases/categorizer";
 import { Category, GameItem } from "@/domain/entities/Game";
 
 export class CategorizerJson implements Categorizer {
+  bigger: number
+  smaller: number
+
   constructor(
     private readonly data: number[][]
-  ){}
+  ){
+    this.bigger = Number.NEGATIVE_INFINITY
+    this.smaller = Number.POSITIVE_INFINITY
+  }
 
   exec(): GameItem[] {
     let temp = new Array(60).fill(0);
@@ -15,22 +21,32 @@ export class CategorizerJson implements Categorizer {
         temp[num - 1]++
       }
     }
+
+    for (let n of temp) {
+      if (n > this.bigger)
+        this.bigger = n
+
+      if (n < this.smaller)
+        this.smaller = n
+    }
+
     for (const [v, sum] of Object.entries(temp)) {
       items.push({
         value: parseInt(v) + 1,
         category: this.getCategory(sum)    
       })
     }
-
+    
+    console.log("BIGGER " + this.bigger)
+    console.log("SMALLER " + this.smaller)
     return items;
   }
 
   private getCategory(value: number): Category {
-    const normSize = this.data.length / 6;
-    const howToDecrease = Math.trunc(normSize / 5); 
+    const howToDecrease: number = (this.bigger - this.smaller) / 5
     
     for (let c = 1; c <= 5; c++) {
-      if (value > (normSize - (howToDecrease * c))) {
+      if (value >= this.bigger - (howToDecrease * c)) {
         return Category[`class${c}`]
       }
     }
